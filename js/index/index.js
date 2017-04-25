4636dabb723e3a70a481b32d6334810c0786e2f4
alan2048 <1055916685@qq.com>
2017-04-25 17:49:16 +0800

    change

--- js/index/index.js
+++ js/index/index.js
@@ -3,15 +3,9 @@ $(function () {
 });
 function winResize() {
 	var fs=$(window).width()/19.2;
-	if(fs >100){
-		fs=100
-	};
 	$("html").css("font-size",fs);
 	$(window).resize(function () {
 		var fs01=$(window).width()/19.2;
-		if(fs01 >100){
-			fs01=100
-		};
 		$("html").css("font-size",fs01);
 	});
 };
